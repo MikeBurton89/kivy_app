a185fd46f372bb393fb05f5904edc371e17ecbcd
MikeBurton89 <40004867+MikeBurton89@users.noreply.github.com>
2021-07-18 10:58:55 +0200

    Update geo_coding.py

--- geo_coding.py
+++ geo_coding.py
@@ -1,16 +1,20 @@
 import googlemaps
 import database_connection
 import sqlite3
-
+#connect to the database in the folder
 connect_database = sqlite3.connect('database_address.db')
+
 cur = connect_database.cursor()
 
-gmaps = googlemaps.Client(key= 'AIzaSyAetuK912epZTR2lG2od8nC8lKlSj39GVo')
+gmaps = googlemaps.Client(key= '#there should be a googlemaps API key')
+
+#this function should add the address to the database. Needs to be expanded to ask for name of the place and other infos
 def address_add():
-    address_input = input("inserisci lindirizzo")
-    cur.execute(INSERT )
+    address_input = input("What's the address?")
+    cur.execute(INSERT INTO address VALUES(address_input)
+    cur.commit()
 
-
+# this function should fetch the address so that it can be used to geocode it
 #def address_fetch():
     #cur.execute('''FROM address SELECT street''')
 
